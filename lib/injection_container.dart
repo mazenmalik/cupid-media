54012b6d23bfd65350a978ed2f10bec3143e5b5a
mazenmalik <mazen.malik@locq.com>
2024-04-28 23:50:25 +0800

    chore: create country bloc

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -3,6 +3,7 @@ import 'package:cupid_media/data/repositories/place_repository_impl.dart';
 import 'package:cupid_media/domain/repositories/place_repository.dart';
 import 'package:cupid_media/domain/usecases/get_countries_usecase.dart';
 import 'package:cupid_media/domain/usecases/get_state_usecase.dart';
+import 'package:cupid_media/presentation/bloc/country/country_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,7 +11,7 @@ final locator = GetIt.instance;
 
 void setupLocator() {
   // bloc
-
+  locator.registerFactory(() => CountryBloc(locator()));
   // usecase
   locator.registerLazySingleton(() => GetCountryUseCase(locator()));
   locator.registerLazySingleton(() => GetStateUseCase(locator()));
