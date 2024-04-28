33b1bd391a06ce81f0740f9cc193caf579df5e31
mazenmalik <mazen.malik@locq.com>
2024-04-29 02:00:18 +0800

    chore: update home file name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cupid_media/injection_container.dart';
 import 'package:cupid_media/presentation/bloc/country/country_bloc.dart';
 import 'package:cupid_media/presentation/bloc/country/country_event.dart';
-import 'package:cupid_media/presentation/pages/home.dart';
+import 'package:cupid_media/presentation/pages/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'presentation/bloc/state/state_bloc.dart';
